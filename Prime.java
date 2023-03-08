f8557fa384bd5921bfef5b1865afe24874388330
IGNECIA RATHNA M <73903767+Igneciarathna@users.noreply.github.com>
2023-03-08 16:33:19 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num=s.nextInt();
+        int count=0;
+        if(num>1) {
+            for(int i=1;i<=num;i++) {
+                if(num%i==0)
+                    count++;
+            }
+            if(count==2) {
+                System.out.println("Prime Number");
+            }else {
+                System.out.println("Not a Prime Number");
+            }
+        }else {
+            System.out.println("Not a Prime Number");
+        }
+
+    }
+}
