9428dac6713719623ada156883f8182f14e50d93
IGNECIA RATHNA M <73903767+Igneciarathna@users.noreply.github.com>
2023-03-09 16:40:56 +0530

    Add files via upload

---
+++ MaxAndMinElementArray.java
@@ -0,0 +1,22 @@
+public class MaxAndMinElementArray {
+    public static void main(String[] args) {
+       /* int[] a={10,40,20,50,100};
+        int max=a[0];
+        for(int i=1;i<a.length;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        System.out.println("Maximum element in array "+max);
+    }
+} */
+    int[] a={10,40,20,50,100};
+    int min=a[0];
+      for(int i=1;i<a.length;i++){
+        if(a[i]<min){
+        min=a[i];
+        }
+        }
+        System.out.println("Minimum element in array "+min);
+        }
+        }
